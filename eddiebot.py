f6b9cc4441ad6d235b963d5c80bf1c97517a6ef5
Christopher Keller <ckeller@datanexus.org>
2017-05-11 07:55:58 +1000

    update algorithm for PRs

--- eddiebot.py
+++ eddiebot.py
@@ -6,13 +6,20 @@ __author__ = 'ckeller@datanexus.org'
 
 class Developer:
     """class that holds the developer commit information"""
-    def __init__(self, name, login, email, additions, deletions):
+    def __init__(self, name, login, email, additions, deletions, merge):
         self.name = name
         self.login = login
         self.email = email
-        self.commits = 1
-        self.additions = additions
-        self.deletions = deletions
+        if merge:
+            self.prs = 1
+            self.commits = 0
+            self.additions = 0
+            self.deletions = 0
+        else:
+            self.prs = 0
+            self.commits = 1
+            self.additions = additions
+            self.deletions = deletions
     
     def setName(self, name):
         self.name = name
@@ -23,17 +30,24 @@ class Developer:
     def setEmail(self, email):
         self.email = email
     
-    def incrementChanges(self, additions, deletions):
-        self.commits = self.commits + 1
-        self.additions = self.additions + additions
-        self.deletions = self.deletions + deletions    
+    def incrementPR(self):
+        self.pr = self.pr + 1
+        
+    def incrementChanges(self, additions, deletions, merge):
+        if merge:
+            # don't add the counts for merges so you don't get double credit for self-merging
+            self.prs = self.prs + 1
+        else:
+            self.commits = self.commits + 1
+            self.additions = self.additions + additions
+            self.deletions = self.deletions + deletions    
             
     def display(self, manager, project):
         """display CSV or standard"""
         if manager:
-            print "{},{},{},{},{},{}".format(project, self.login, self.name, self.commits, self.additions, self.deletions)
+            print "{},{},{},{},{},{},{}".format(project, self.login, self.name, self.commits, self.prs, self.additions, self.deletions)
         else:
-            print "\t{} ({}): {} commits {} additions {} deletions".format(self.login, self.name, self.commits, self.additions, self.deletions)
+            print "\t{} ({}): {} commits {} merges {} additions {} deletions".format(self.login, self.name, self.commits, self.prs, self.additions, self.deletions)
             
     def __hash__(self):
         return hash((self.login))
@@ -57,30 +71,38 @@ def processCommits(repo, args):
 
     try:
         for commit in repo.get_commits():
+            merge = False
             # just in case all the user data isn't fully populated (should only happen in enterprise GitHub)
             if commit.author is None:
-                author='unassigned'
-                login='s0'
+                name='nobody'
+            else:
+                name = commit.author.name
+            
+            if commit.committer is None:
+                login = 's0'
+            else:
+                login = commit.committer.login
+                
+            if commit.committer is None:
+                # commit is a PR merge
+                merge = True
+                # wierd case of bad data
+                if commit.author is None:
+                    login = 's0'
+                else:
+                    login = commit.author.login
+        
+            if commit.author is None or commit.author.email is None:
                 email='noone@iag.com.au'
             else:
-                author = commit.author.name
-        
-                if commit.committer is None:
-                    login = commit.author.login
-                else:
-                    login = commit.committer.login
-            
-                if commit.author.email is None:
-                    email='noone@iag.com.au'
-                else:
-                    email=commit.author.email
+                email=commit.author.email
    
-            # create new developer commit object
-            x = Developer(author, login, email, commit.stats.additions, commit.stats.deletions)
+            # create new developer commit object            
+            x = Developer(name, login, email, commit.stats.additions, commit.stats.deletions, merge)
         
             # either add it to the hash or update existing    
             if x in people:
-                people[x].incrementChanges(commit.stats.additions, commit.stats.deletions)
+                people[x].incrementChanges(commit.stats.additions, commit.stats.deletions, merge)
             else:
                 people[x] = x
             counter += 1
@@ -91,7 +113,8 @@ def processCommits(repo, args):
         for k in sorted(people):
             k.display(args.manager, repo.full_name)
     # repos with 0 commits generate an exception which we can safely ignore
-    except: GithubException
+    except GithubException as e:
+        print "caught exception {}".format(e)
                     
 def main():
     """main routine"""
@@ -122,7 +145,7 @@ def main():
     if args.repo:
         processCommits(g.get_repo(args.repo), args)
         sys.exit(0)
-        
+  
     # brute force walk every commit in every repo accessible
     for repo in g.get_repos():
         processCommits(repo, args)
